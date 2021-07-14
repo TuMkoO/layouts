b9ee81526a5a32069bd450ff457768252d60980e
TuMkoO <mr.t09@mail.ru>
2021-07-14 21:31:42 +0300

    update

--- test/js/main.js
+++ test/js/main.js
@@ -423,6 +423,7 @@
         });
 
         if ( $(window).width() < 575 ) {
+          // console.log($(window).width());
           $('.select2-search__field').blur();
         }
 
@@ -456,6 +457,23 @@
     addSelect2('.select2-transmission', true);
     addSelect2('.select2-run-drive', true);
     addSelect2('.select2-starts', true);
+
+
+    /*----------------------------------------------------*/
+    /*  vh
+    /*----------------------------------------------------*/
+
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
 
 
     /*----------------------------------------------------*/
