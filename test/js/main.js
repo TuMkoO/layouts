938c844f1c9996a690397fa9faf1b819b35e014d
TuMkoO <mr.t09@mail.ru>
2021-07-25 15:35:27 +0300

    update

--- test/js/main.js
+++ test/js/main.js
@@ -463,7 +463,7 @@
     /*----------------------------------------------------*/
     /*  100vh fix
     /*----------------------------------------------------*/
-/*
+
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     let vh = window.innerHeight * 0.01;
     // Then we set the value in the --vh custom property to the root of the document
@@ -475,7 +475,7 @@
       let vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
-*/
+
 
     /*----------------------------------------------------*/
     /*  Filters Show More
